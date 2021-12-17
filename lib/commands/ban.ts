503c3331addf37f8e293701cfa4b81969a50ba3d
Rodrigo Souto <rodrigo_7@live.com.ar>
2021-12-17 19:46:58 -0300

    /ban [WIP]

--- lib/commands/ban.ts
+++ lib/commands/ban.ts
@@ -2,13 +2,44 @@ import * as TelegramBot from "node-telegram-bot-api";
 import {CommandCallback} from "../../types";
 
 /*
- * /ban ?
+ * /ban
  */
-const regexp = /\/ban (.+)/
+const regexp = /\/ban/
 
-const callback: CommandCallback = (bot: TelegramBot, msg: TelegramBot.Message, match: string[]) => {
-    // TODO
-    bot.sendMessage(msg.chat.id, 'This command is not implemented yet...');
+const callback: CommandCallback = async (bot: TelegramBot, msg: TelegramBot.Message) => {
+
+    if (!msg.reply_to_message) {
+        await bot.sendMessage(msg.chat.id, `/ban must be used in a reply`);
+        return;
+    }
+
+    const botUser = await bot.getMe();
+
+    if (botUser.id === msg.reply_to_message.from.id) {
+        await bot.sendMessage(msg.chat.id, `The Moderator Bot can't be banned.`);
+        return;
+    }
+
+    const user = await bot.getChatMember(msg.chat.id, String(msg.from.id));
+
+    //TODO: evidence is saved (how is it saved? What evidence?)
+
+    if (user.status === 'creator' || user.status === 'administrator') {
+        // TODO
+        // A question is automatically created in Realitio with an answer in favor of banning the user.
+        // Bond of the answer: 1 xDAI (initially the answer can be omitted).
+    } else {
+        // TODO
+        // The user is required to create a question in Realitio and submit an answer with a minimum bond.
+    }
+
+    // the user gets notified and it is explained to them how to appeal.
+    await bot.sendMessage(msg.chat.id, `*${msg.reply_to_message.from.username}* you have been banned, you can appeal here: {URL}`, {parse_mode: 'Markdown'});
+
+    // TODO: If the user appeals (submits an answer to Realitio), the user gets automatically unbanned.
+
+    //@ts-ignore
+    await bot.banChatMember(msg.chat.id, String(msg.reply_to_message.from.id));
 }
 
 export {regexp, callback};
