577de300f4097b9a241de1ae3debb3d83f542261
Rodrigo Souto <rodrigo_7@live.com.ar>
2021-12-20 13:45:49 -0300

    allow to quote a msg to set rules / validate url

--- lib/commands/setRules.ts
+++ lib/commands/setRules.ts
@@ -1,19 +1,42 @@
 import * as TelegramBot from "node-telegram-bot-api";
 import {CommandCallback} from "../../types";
 import {setRules} from "../db";
+import ipfsPublish from "../ipfs-publish";
 
 /*
  * /setrules [ipfs file path]
  */
-const regexp = /\/setrules (.+)/
+const regexp = /\/setrules\s?(.+)?/
+
+const validateUrl = (s: string): boolean => {
+    try {
+        new URL(s);
+        return true;
+    } catch (err) {
+        return false;
+    }
+};
 
 const callback: CommandCallback = async (bot: TelegramBot, msg: TelegramBot.Message, match: string[]) => {
-    // TODO: validate url?
     const user = await bot.getChatMember(msg.chat.id, String(msg.from.id));
 
     if (user.status === 'creator' || user.status === 'administrator') {
-        await setRules(msg.chat.id, match[1]);
-        await bot.sendMessage(msg.chat.id, 'Rules updated');
+        if (msg.reply_to_message) {
+            const enc = new TextEncoder();
+
+            const rulesPath = await ipfsPublish('rules.json', enc.encode(msg.reply_to_message.text))
+
+            await setRules(msg.chat.id, rulesPath);
+
+            await bot.sendMessage(msg.chat.id, 'Rules updated');
+        } else {
+            if (validateUrl(match[1])) {
+                await setRules(msg.chat.id, match[1]);
+                await bot.sendMessage(msg.chat.id, 'Rules updated');
+            } else {
+                await bot.sendMessage(msg.chat.id, 'Invalid url passed to /setrules');
+            }
+        }
     } else {
         await bot.sendMessage(msg.chat.id, `Only admins can execute this command.`);
     }
---
+++ lib/ipfs-publish.ts
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch';
+
+/**
+ * Send file to IPFS network via the Kleros IPFS node
+ * @param {string} fileName - The name that will be used to store the file. This is useful to preserve extension type.
+ * @param {ArrayBuffer} data - The raw data from the file to upload.
+ * @return {string} URL of the stored item.
+ */
+const ipfsPublish = async (fileName, data): Promise<string> => {
+    const buffer = await Buffer.from(data)
+
+    const response = await fetch('https://ipfs.kleros.io/add', {
+        method: 'POST',
+        body: JSON.stringify({
+            fileName,
+            buffer
+        }),
+        headers: {
+            'content-type': 'application/json'
+        }
+    });
+
+    const body = await response.json();
+
+    return `https://ipfs.kleros.io/ipfs/${body.data[1].hash}${body.data[0].path}`
+}
+
+export default ipfsPublish
