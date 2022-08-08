a59165643fb2d9428dbddbb150d675e220aa9ba1
shotaronowhere <shawtarohgn@gmail.com>
2022-08-08 14:08:16 +0200

    feat(db): refactoring

--- lib/create-db.ts
+++ lib/create-db.ts
@@ -4,41 +4,40 @@ import {openDb} from "./db";
     const db = await openDb();
 
     /**
-     * `app_type` + `app_user_id` is used to verify if the user is the owner of the bot.
+     * `platform` + `platform_user_id` is used to verify if the user is the owner of the bot.
      *
      * The owner is the only user who can call `/setaccount` to use this bot in a group.
      *
-     * `app_type` can be `telegram`, `reddit`, etc.
-     * `app_user_id` is the id of the user in `app_type`.
+     * `platform` can be `telegram`, `reddit`, etc.
+     * `platform_user_id` is the id of the user in `app_type`.
      */
-    await db.exec('CREATE TABLE bots (address TEXT PRIMARY KEY, private_key TEXT, app_type TEXT, app_user_id TEXT)');
+     await db.exec('CREATE TABLE accounts (address TEXT PRIMARY KEY, private_key TEXT, platform TEXT, user_id TEXT)');
 
     /**
      * A group can be a telegram chat, a subreddit, etc.
      *
-     * `app_type` can be `telegram`, `reddit`, etc.
-     * `app_group_id` is the id of the telegram group or the name of the subreddit.
+     * `platform` can be `telegram`, `reddit`, etc.
+     * `group_id` is the id of the telegram group or the name of the subreddit.
      * `address` is the address of the bot assigned to this group.
      */
-    await db.exec('CREATE TABLE bot_groups (app_type TEXT, app_group_id TEXT, address TEXT, PRIMARY KEY (app_type, app_group_id))');
-
+    await db.exec('CREATE TABLE groups (platform TEXT, group_id TEXT, invite_url TEXT, address TEXT, PRIMARY KEY (platform, group_id))');
 
     /**
      * `question_id` is the id of the question in reality.eth
-     * `app_type` can be `telegram`, `reddit`, etc.
-     * `app_group_id` is the id of the telegram group or the name of the subreddit.
-     * `app_user_id` is the id of the banned user in `app_type`.
+     * `platform` can be `telegram`, `reddit`, etc.
+     * `group_id` is the id of the group on the platform (eg. group on telegram, subreddit on reddit, etc.).
+     * `user_id` is the id of the banned user in `platform`.
+     * `chat_id` is the id of the banned user in `platform`.
      * `active` indicates whether the user is currently banned.
-     * `finalized` indicates if the question has expired.
+     * `finalized` indicates if the question has finalized.
      */
-    await db.exec('CREATE TABLE bans (question_id TEXT PRIMARY KEY, app_type TEXT, app_group_id TEXT, app_user_id TEXT, active BOOLEAN, finalized BOOLEAN)');
-
-    /*
-     * The following tables are only used for telegram
+    await db.exec('CREATE TABLE reports (question_id TEXT PRIMARY KEY, platform TEXT, group_id TEXT, user_id TEXT, msg_id TEXT, timestamp INTEGER, activeTimestamp INTEGER, active BOOLEAN, timeServed INTEGER, finalized BOOLEAN, evidenceAllowance INTEGER)');
+    /**
+     * `platform` can be `telegram`, `reddit`, etc.
+     * `group_id` is the id of the group on the platform (eg. group on telegram, subreddit on reddit, etc.).
+     * `rules` the group rules. eg ipfs cid string.
+     * `timestamp` the timestamp when the rules were set.
      */
-    await db.exec('CREATE TABLE rules (chat_id INTEGER PRIMARY KEY, rules TEXT)');
+    await db.exec('CREATE TABLE rules (platform TEXT, group_id TEXT, rules TEXT, timestamp INTEGER, PRIMARY KEY (platform, group_id))');
 
-    await db.exec('CREATE TABLE mods (chat_id INTEGER, user_id INTEGER, PRIMARY KEY (chat_id, user_id))');
-
-    console.log('database created');
 })();
