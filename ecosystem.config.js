33b8a3079ef08f81d9d9569a4a8f258613e7ac12
shotaronowhere <shawtarohgn@gmail.com>
2022-08-12 21:07:47 +0200

    chore(pm2): fix yarn error

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,16 +3,25 @@ module.exports = {
         name: 'moderator_telegram_bot',
         script: 'yarn',
         args: 'start-telegram-bot',
+        interpreter: '/bin/bash',
         log_date_format : 'YYYY-MM-DD HH:mm Z',
         watch: true,
         ignore_watch: ['\.git', 'node_modules', 'database\.db', 'database\.db-journal'],
+        env: {
+            NODE_ENV: 'development'
+          }
     }, {
         name: 'moderator_cron',
         script: 'yarn',
         args: 'cron',
+        interpreter: '/bin/bash',
         log_date_format : 'YYYY-MM-DD HH:mm Z',
+        autorestart: false,
         watch: true,
         ignore_watch: ['\.git', 'node_modules', 'database\.db', 'database\.db-journal'],
         cron_restart: '* * * * *',
+        env: {
+            NODE_ENV: 'development'
+          }
     }],
 };
