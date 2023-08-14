ce77730fed1683a2cd39ad5f6729d161a47c8cdf
shotaronowhere <shawtarohgn@gmail.com>
2023-08-14 19:54:18 +0100

    fix: no web preview

--- lib/telegram/commands/addEvidence.ts
+++ lib/telegram/commands/addEvidence.ts
@@ -40,7 +40,7 @@ const processCommand = async (queue: any, bot: any, settings: groupSettings, msg
     const evidenceJsonPath = await uploadEvidenceJson(settings.lang, msg, evidencePath,isPrivate);
     try{
         queue.add(async () => {try{
-            const resp = await bot.sendMessage(msg.chat.id, `${langJson[settings.lang].addevidence.submitted}(https://ipfs.kleros.io${evidencePath}).`, msg.is_topic_message? {parse_mode: "Markdown", message_thread_id: msg.message_thread_id}:{parse_mode: "Markdown"})
+            const resp = await bot.sendMessage(msg.chat.id, `${langJson[settings.lang].addevidence.submitted}(https://ipfs.kleros.io${evidencePath}).`, msg.is_topic_message? {parse_mode: "Markdown", message_thread_id: msg.message_thread_id}:{parse_mode: "Markdown",  disable_web_page_preview: true})
             return resp.message_id}catch{}});
     } catch(e){
         console.log(e)
